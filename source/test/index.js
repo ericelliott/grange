5455ec631acf2446dff92bc60f3a8b4b747f5485
Eric Elliott <support@paralleldrive.com>
2016-05-18 17:16:11 -0600

    Implement simple range.

--- source/test/index.js
+++ source/test/index.js
@@ -1,1 +1,12 @@
-throw new Error('No tests!');
+const test = require('tape');
+const grange = require('../grange');
+
+test('simple number range', assert => {
+  const msg = 'should produce a simple range';
+
+  const [...actual] = grange(3, 6);
+  const expected = [3, 4, 5, 6];
+
+  assert.same(actual, expected, msg);
+  assert.end();
+});
